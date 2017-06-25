82f7fa7829ef6d5e1e5290c559f2f5b436e270ef
naukmamh <demonpro100@i.ua>
2017-06-26 00:48:09 +0300

    Update AppConfig.java

--- src/main/java/com/unixteam/AppConfig.java
+++ src/main/java/com/unixteam/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import java.lang.System;
 
 
 
